ffecfbd3d9ff7b4a4341637e9a8233c2a43ad7d9
Przemek Pastuszka <pastuszka.przemyslaw@gmail.com>
2013-06-08 08:09:22 +0200

    more and more javadoc

--- src/main/java/pl/rtshadow/bezier/curve/transformations/LeastSquaresReduction.java
+++ src/main/java/pl/rtshadow/bezier/curve/transformations/LeastSquaresReduction.java
@@ -4,6 +4,7 @@
 
 package pl.rtshadow.bezier.curve.transformations;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.Lists.newArrayListWithCapacity;
 
 import java.util.List;
@@ -15,26 +16,44 @@ import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.linear.SingularValueDecomposition;
 
-import pl.rtshadow.bezier.util.Coordinate;
 import pl.rtshadow.bezier.util.BoundedIterable;
+import pl.rtshadow.bezier.util.Coordinate;
 
+/**
+ * Performs degree reduction of Bezier curve using matrix representation of
+ * inversion of degree elevation process.
+ * 
+ * Elevation to higher degree curve with B coeffs from lower degree curve
+ * with B' coeffs can be represented as linear algebra equation:
+ * M * B' = B
+ * In case of degree reduction B' is unknown variable
+ * 
+ * This is most expensive of implemented methods (in terms of big O notation), but most accurate
+ */
 public class LeastSquaresReduction implements BezierTransformation {
+
+  /**
+   * Uses SVD decomposition of M matrix to perform the computations
+   */
   @Override
   public List<Coordinate> apply(BoundedIterable<Coordinate> controlPoints) {
-    // M^T * M * Bnew = M^T * Bold
+    checkArgument(controlPoints.getSize() > 2);
 
     int n = controlPoints.getSize() - 1;
+
     RealMatrix M = prepareMMatrix(n);
     DecompositionSolver solver = new SingularValueDecomposition(M).getSolver();
-
     RealVector targetXs = solver.solve(getXs(controlPoints));
     RealVector targetYs = solver.solve(getYs(controlPoints));
 
+    return join(targetXs, targetYs, n);
+  }
+
+  private List<Coordinate> join(RealVector targetXs, RealVector targetYs, int n) {
     List<Coordinate> newControlPoints = newArrayListWithCapacity(n);
     for (int i = 0; i < n; ++i) {
       newControlPoints.add(new Coordinate(targetXs.getEntry(i), targetYs.getEntry(i)));
     }
-
     return newControlPoints;
   }
 
@@ -58,6 +77,10 @@ public class LeastSquaresReduction implements BezierTransformation {
     return new ArrayRealVector(xs);
   }
 
+  /**
+   * This is a matrix representation of the equation described in javadoc
+   * to {@link DegreeElevationInversion}.
+   */
   private RealMatrix prepareMMatrix(int n) {
     RealMatrix M = new Array2DRowRealMatrix(n + 1, n);
 
